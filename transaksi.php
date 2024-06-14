8f02a41f229cdf98e727e52af985889f4cfe1b5c
Adilla Aulia <112401483+adillaads@users.noreply.github.com>
2024-06-14 08:35:01 +0700

    Add files via upload

--- transaksi.php
+++ transaksi.php
@@ -1,3 +1,13 @@
+<?php 
+
+include 'konfigurasi.php';
+
+    
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header('Location: login.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
